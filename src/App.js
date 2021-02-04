1cffa83c26024ac4ac67ea722bd271eba0b51c01
Maxi Diaz <maxi.diaz66@gmail.com>
2021-02-04 20:37:21 -0300

    Se modifico el background de la pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
     <Fragment>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
       {/* HEADER */}
       <header className="d-flex justify-content-center text-center">
-      <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
+        <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
       </header>
 
     </Fragment>
