8d8a23fc82ba7abd70e57a5ac9aa7f769d49c93f
Maxi Diaz <maxi.diaz66@gmail.com>
2021-02-04 18:25:45 -0300

    Inicio del proyecto: Se agrego Bootstrap y se definieron algunos estilos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <header className="d-flex justify-content-center text-center">
+      <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
+      </header>
+    </Fragment>
+  );
+}
+
+export default App;
