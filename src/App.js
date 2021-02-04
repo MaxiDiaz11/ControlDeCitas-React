ffee611360df70640e839fc99d920ee323e332cd
Maxi Diaz <maxi.diaz66@gmail.com>
2021-02-04 18:42:01 -0300

    Se agrego un contenedor con una fila y dos columnas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
     <Fragment>
       {/* HEADER */}
       <header className="d-flex justify-content-center text-center">
-      <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
+        <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
       </header>
 
+
+      {/* SECCION TURNOS */}
+      <main>
+        <section className="container">
+          <div className="row">
+            <div className="col-6">
+              <h1>Hola mundo</h1>
+            </div>
+            <div className="col-6">
+              <h1>Hola mundo</h1>
+            </div>
+          </div>
+        </section>
+      </main>
     </Fragment>
   );
 }
