626b72514ec3f4ec08aaf8eae5b20f25f435a3fd
Maxi Diaz <maxi.diaz66@gmail.com>
2021-02-04 18:30:35 -0300

    Se agrego el header junto un titulo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React, {Fragment} from 'react';
 function App() {
   return (
     <Fragment>
+      {/* HEADER */}
       <header className="d-flex justify-content-center text-center">
       <h1 className="my-5 text-dark titulo px-4">Control de citas</h1>
       </header>
+
     </Fragment>
   );
 }
